a13d902037a15421c9e1116e5149099eab611a35
lopeselio <lopeselio@gmail.com>
2023-09-24 18:47:17 +0530

    walking and idle transition complete

--- Assets/Game/Scripts/Move2D.cs
+++ Assets/Game/Scripts/Move2D.cs
@@ -7,14 +7,17 @@ public class Move2D : MonoBehaviour
 {
     public float m_speed = 1.0f;
     private Rigidbody2D _rigidbody2D;
+    private Animator _animator;
     private Vector2 _userinput;
 
     void Awake() {
         _rigidbody2D = GetComponent<Rigidbody2D>();
+        _animator = GetComponent<Animator>();
     }
 
     public void Move(InputAction.CallbackContext context){
         _userinput = context.ReadValue<Vector2>();
+        _animator.SetFloat("speed", _userinput.magnitude);
     }
 
     void FixedUpdate () {
