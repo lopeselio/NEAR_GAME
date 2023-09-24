54110a8e4b074a9d567caf8612b6d9bdb3302652
lopeselio <lopeselio@gmail.com>
2023-09-24 18:54:10 +0530

    flip animation: change direction

--- Assets/Game/Scripts/Move2D.cs
+++ Assets/Game/Scripts/Move2D.cs
@@ -9,6 +9,7 @@ public class Move2D : MonoBehaviour
     private Rigidbody2D _rigidbody2D;
     private Animator _animator;
     private Vector2 _userinput;
+    private bool _flipped;
 
     void Awake() {
         _rigidbody2D = GetComponent<Rigidbody2D>();
@@ -18,6 +19,13 @@ public class Move2D : MonoBehaviour
     public void Move(InputAction.CallbackContext context){
         _userinput = context.ReadValue<Vector2>();
         _animator.SetFloat("speed", _userinput.magnitude);
+        if(_userinput.x < 0 && !_flipped){
+            _flipped = true;
+        }
+        else if(_userinput.x >0 && _flipped){
+            _flipped = false;
+        }
+        transform.localScale = new Vector3(_flipped ? -1: 1, 1, 1);
     }
 
     void FixedUpdate () {
